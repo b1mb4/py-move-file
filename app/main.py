fb8110af26cfc070fc176be6d8720233f7b167a9
artemkliuienkov <artemkluenkov41@gmail.com>
2026-03-24 20:01:38 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    _, source_path, destination_path = command.split()
+
+    if destination_path.endswith("/"):
+        create_directories(destination_path.rstrip("/"))
+        destination_path = f"{destination_path}{os.path.basename(source_path)}"
+    else:
+        destination_directory = os.path.dirname(destination_path)
+        if destination_directory:
+            create_directories(destination_directory)
+
+    with open(source_path, "r") as source_file:
+        file_content = source_file.read()
+
+    with open(destination_path, "w") as destination_file:
+        destination_file.write(file_content)
+
+    os.remove(source_path)
+
+
+def create_directories(path: str) -> None:
+    current_path = ""
+
+    for directory in path.split("/"):
+        current_path = os.path.join(current_path, directory)
+        if not os.path.exists(current_path):
+            os.mkdir(current_path)
